58848a4704e9b20039e89ca656b263b304b4c192
CrazyDuck22 <crazydusk22@gmail.com>
2023-01-06 12:34:23 +0100

    updated people!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include "headers/ship.h"
-#include "headers/player.h"
 #include <iostream>
+#include <string.h>
 
 int main(int argc, char **argv){
     srand(time(0));
+
+    /*
+    ------ DISTINZIONE DEGLI ESEGUIBILI ------              //todo: testare
+    if(strcmp(argv[0], "battaglia_navale") == 0){
+        std::cout << "** PARTITA BATTAGLIA NAVALE **" << std::endl;
+        if(strcmp(argv[1], "pc") == 0) std::cout << "PARTITA CON GIOCATORE VS COMPUTER" << std::endl;
+        if(strcmp(argv[0], "cc") == 0) std::cout << "PARTITA CON COMPUTER VS COMPUTER" << std::endl;
+    }
+    else{
+        if(strcmp(argv[0], "replay") == 0){
+            std::cout << "** REPLAY **" << std::endl;
+        }
+        else{
+            std::cout << "** IMPOSSIBILE ESEGUIRE IL PROGRAMMA RICHIESTO | CHIUSURA DEL PROGRAMMA **" << std::endl;
+        }
+    }
+     */
+
+
     /*int d = 1;
     for (int i = 0; i < 3; ++i) {
 
