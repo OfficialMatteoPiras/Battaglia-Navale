d58f027b584b22ec8bfae2af978e1ef224cd8db8
CrazyDuck22 <crazydusk22@gmail.com>
2023-01-18 21:09:41 +0100

    updated people!

--- headers/matrix.h
+++ headers/matrix.h
@@ -12,24 +12,25 @@ private:
     char _matrix[12][12] = {};
 
 public:
+    //costruttore
     matrix();
 
     //insert
-    void insert(coords, char);
+    void insert(coords, char);      //inserische un carattere nella matrice
 
     //getters
-    char getElement(coords) const;
-    char getElement(int row, int col) const;
-    bool isEmpty(coords target) const;
+    char getElement(coords) const;      //restituisce un elemento date le sue coordinate
+    char getElement(int row, int col) const;    //overload: restituisce un elemento data riga e colonna
+    bool isEmpty(coords target) const;      //date le coordinate, restituisce se la cella è ha un elemento o meno (vuota o piena)
     int getShipDim(coords c) const;     //restituisce la dimensione della nave nelle coordinate c (in base alla lettera)
-    std::vector<coords> findLetter(char letter) const;
+    std::vector<coords> findLetter(char letter) const;      //funzione che data una lettara restituisce un vettore con le tutte le coordinate in cui è stata trovata nella matrice
 
     //setters
-    void hit(coords target);
-    void restore(coords target);
+    void hit(coords target);                //setta la lettara nelle coordinate date in minuscolo
+    void restore(coords target);            //setta la lettara nelle coordinate date in maiuscolo
 };
 
 //stampa due matrici affiancate
-void grid(matrix ships, matrix attack);
+void grid(matrix ships, matrix attack);             //stampa delle griglie affiancate
 
 #endif //BATTAGLIA_NAVALE_MATRIX_H
